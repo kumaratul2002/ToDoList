db33dc522b6c96a3dfb35ca77480e7699934a320
Atul <doodleforstudy@gmail.com>
2023-06-11 16:13:37 +0530

    Updation in To do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
 
 
 const App=()=> {
+  const[InputList,setInputList]=useState("")
+  const[items,setitems]=useState([]);                  //    it makes it a array
+  const itemEvent=(event)=>{
+    setInputList(event.target.value);
+  }
+  const listofitems=()=>{
+         setitems((val)=>{
+          return [...val,InputList];
+         });
+         setInputList("");
+  }
   return (
     <>
       <div className='main_div'>
@@ -10,11 +22,14 @@ const App=()=> {
           <br/>
           <h1>ToDo list</h1>
           <br/>
-          <input type="text" placeholder="Add"/>
-          <button>+</button>
+          <input type="text" placeholder="Add" value={InputList} onChange={itemEvent}/>   
+          <button onClick={listofitems}>+</button>
 
           <ol>
-            <li> Buy Apple</li>
+            {/* <li>{InputList}</li> */}
+            {items.map((itemval)=>{
+               return <ToDoList text={itemval}/>
+            })}
           </ol>
         </div>
       </div>
