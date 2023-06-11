1957401b0527422b2eda7d5d738f83d4657c19df
Atul <doodleforstudy@gmail.com>
2023-06-11 16:46:15 +0530

    Final with delete option

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,53 @@
-import React, { useState } from 'react';
-import ToDoList from './ToDoList';
+import React, { useState } from "react";
+import ToDoList from "./ToDoList";
 
-
-
-const App=()=> {
-  const[InputList,setInputList]=useState("")
-  const[items,setitems]=useState([]);                  //    it makes it a array
-  const itemEvent=(event)=>{
+const App = () => {
+  const [InputList, setInputList] = useState("");
+  const [items, setitems] = useState([]); //    it makes it a array
+  const itemEvent = (event) => {
     setInputList(event.target.value);
-  }
-  const listofitems=()=>{
-         setitems((val)=>{
-          return [...val,InputList];
-         });
-         setInputList("");
-  }
+  };
+
+
+  const listofitems = () => {
+    setitems((val) => {
+      return [...val, InputList];
+    });
+    setInputList("");
+  };
+
+  const deleteitems = (id) => {
+      setitems((val)=>{
+         return val.filter((arr,index)=>{
+          return index !== id;
+                 });
+      });
+  };
   return (
     <>
-      <div className='main_div'>
-        <div className='center_div'>
-          <br/>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
           <h1>ToDo list</h1>
-          <br/>
-          <input type="text" placeholder="Add" value={InputList} onChange={itemEvent}/>   
+          <br />
+          <input
+            type="text"
+            placeholder="Add"
+            value={InputList}
+            onChange={itemEvent}
+          />
           <button onClick={listofitems}>+</button>
 
           <ol>
             {/* <li>{InputList}</li> */}
-            {items.map((itemval)=>{
-               return <ToDoList text={itemval}/>
+            {items.map((itemval, index) => {
+              return <ToDoList text={itemval} key={index} id={index} onSelect={deleteitems}/>;
             })}
           </ol>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const ToDoList=(props)=>{
-    return (
-        <>
-    <li>{props.text}</li>
+const ToDoList = (props) => {
+  
+  return (
+    <>
+      <div className="todo_style">
+        <i
+          className="fa fa-times"
+          aria-hidden="true"
+          onClick={()=>{
+            props.onSelect(props.id);
+          }}
+        />
+        <li>{props.text}</li>
+      </div>
     </>
-    )
-}
+  );
+};
 export default ToDoList;
